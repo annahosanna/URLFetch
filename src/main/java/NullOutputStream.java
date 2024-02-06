70b0542fcbc96dc2bac34ea069d68513a0ec1c9d
annahosanna <Anna.Hosanna@gmail.com>
2024-02-06 13:22:33 -0500

    Update NullOutputStream.java

--- src/main/java/NullOutputStream.java
+++ src/main/java/NullOutputStream.java
@@ -29,35 +29,4 @@ public class NullOutputStream implements OutputStream {
       return "NullOutputStream";
     }
 
-}import io.OutputStream;
-
-public class NullOutputStream implements OutputStream {
-
-    @Override
-    public void write(int b) {
-    }
-
-    // This is supposed to have the same behavior
-    // as the similar method and therefore also can
-    // return an NPE
-    @Override
-    public void write(byte[] b) {
-      if (b == null) {
-        throw new NullPointerException();
-      }
-    }
-
-    // This can return a possible NPE
-    @Override
-    public void write(byte[] b, int off, int len) {
-      if (b == null) {
-        throw new NullPointerException();
-      }
-    }
-
-    @Override
-    public String toString() {
-      return "NullOutputStream";
-    }
-
 }
